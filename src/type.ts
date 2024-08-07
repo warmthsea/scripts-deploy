7e8e4cf76866fbb99576047c7612b7a43714e310
warmthsea <2586244885@qq.com>
2024-08-07 15:30:19 +0800

    types: add content

--- src/type.ts
+++ src/type.ts
@@ -1,9 +1,17 @@
 export interface ScriptsDeployOption {
+  /** Server host */
   host: string
+  /** Server post */
   port: number
+  /** Server login username */
   username: string
+  /** Server login password */
   password: string
+  /** Server folder path */
   wwwPath: string
+  /** Build output folder */
   rootDir: string
+  /** Confirm execution */
+  confirm?: boolean
   default?: Omit<ScriptsDeployOption, 'default'>
 }
